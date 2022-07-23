7d7d90cd617753e920f10cb7804a9adf873bbe2c
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-07-23 16:52:34 -0400

    cleanup

--- robot-arm/utils/get_joint_coords.py
+++ robot-arm/utils/get_joint_coords.py
@@ -18,14 +18,6 @@ def get_joint_coords(lengths: tuple | list,
     >>> get_joint_coords(lengths=(1,), angles=[0], precision=None) #without mapping
     [[0, 0], [1, 0]]
 
-    # >>> get_joint_coords(lengths=(1,), angles=[90], precision=None) #with mapping
-    # [[0, 0], [1, 0]]
-    #
-    # # Example 1a: 1 segment, horizontal
-    # >>> get_joint_coords(lengths=(1,), angles=[90], precision=None)
-    >>> get_joint_coords(lengths=(1,), angles=[90], precision=None)
-    [[0, 0], [0, 1]]
-
     # Example 2: 2 segments, vertical
     >>> get_joint_coords(lengths=[1, 1],angles=[90, 0], precision=None)
     [[0, 0], [0, 1], [0, 2]]
@@ -34,14 +26,11 @@ def get_joint_coords(lengths: tuple | list,
     >>> get_joint_coords((1,),[180],origin=(1,0), precision=None)
     [[1, 0], [0, 0]]
 
-    # Example 4: 2 segments
+    # Example 4: 3 segments
     >>> get_joint_coords(lengths=(1, 1, 1), angles=[90, -90, -90], precision=None)
     [[0, 0], [0, 1], [1, 1], [1, 0]]
     """
     assert len(lengths) == len(angles), "Expecting same number of lengths and angles"
-    input_angles = angles
-    # angles = [interp(a, [0, 180], [-90, 90]) for a in angles]
-    # angles = [interp(a, [-90, 90], [0, 180]) for a in angles]
 
     joint_coords = [list(origin)]  # joint_coords = [[0, 0]]
     for i in range(1, len(lengths) + 1):  # for i in 1, 2, ... , n (number of links) = (1, 2)
